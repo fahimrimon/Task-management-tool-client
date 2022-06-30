1370610803f1619bd70b49de9df9cb05714f83a2
Fahim Rimon <fahimrimon21@gmail.com>
2022-06-30 15:52:30 +0600

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+
+  const menuItems = (
+    <>
+      <li>
+        <Link to="/">Completed Tasks</Link>
+      </li>
+      <li>
+        <Link to="/blogs">To-Do</Link>
+      </li>
+      <li>
+        <Link to="/portfolio">Calendar</Link>
+      </li>
+    </>
+  );
+  return (
+    <div>
+      <div className="navbar bg-secondary ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            {menuItems}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-white normal-case text-xl">My Task-Tool</a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="text-white menu menu-horizontal p-0">{menuItems}</ul>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#02198B",
+          secondary: "#253DA1",
+          accent: "#000137",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
