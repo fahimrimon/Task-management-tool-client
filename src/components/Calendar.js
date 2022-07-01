056beb882d7d5f4c7732ee9c12cbeb5c48077b28
Fahim Rimon <fahimrimon21@gmail.com>
2022-07-01 08:48:07 +0600

    Calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
+import React, { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 const Calendar = () => {
+    const [date, setDate] = useState( Date());
   return (
     <div class="hero min-h-screen">
       <div class="hero-content text-center">
         <div class="max-w-md">
-          <h1 class="text-5xl font-bold">Hello there</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque
-            aut repudiandae et a id nisi.
-          </p>
+            <h1 className="text-amber-400 my-8 text-xl font-bold">Calendar</h1>
+          <DayPicker
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          />
         </div>
       </div>
     </div>
